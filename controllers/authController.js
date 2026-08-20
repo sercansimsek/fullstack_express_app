3bbdbbbe2c4f1467dd43f11b187a72a32aedc1e3
Sercan <sercansimsek44@gmail.com>
2026-08-20 10:56:07 +0200

    C:/Program Files/Git/register route and user validation implemented

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+export const registerUser = (req, res) => {
+  let { name, email, username, password } = req.body;
+
+  if (!name || !email || !username || !password) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
+  name = name.trim();
+  email = email.trim();
+  username = username.trim();
+
+  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+    return res.status(400).json({
+      error:
+        "Username must be 1–20 characters, using letters, numbers, _ or -.",
+    });
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+
+  console.log(req.body);
+};
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { productsRouter } from "./routes/products.js";
+import { authRouter } from "./routes/auth.js";
 
 const PORT = 8000;
 
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static("public"));
 app.use("/api/products", productsRouter);
+app.use("/api/auth", authRouter);
 
 app
   .listen(PORT, () => console.log(`Server is running on the port ${PORT}`))
